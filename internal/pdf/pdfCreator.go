9b4383d6c7a71b385770ef71feacdfcf4d054ee7
Martin Knoller Stocker <linuxswords@gmail.com>
2025-06-07 15:07:44 +0200

    fix(pdf) add font files; german umlaute are working

--- internal/pdf/pdfCreator.go
+++ internal/pdf/pdfCreator.go
@@ -11,9 +11,14 @@ import (
 
 func CreatePDF(db *gorm.DB, scores []models.Score) ([]byte, error) {
 	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.AddUTF8Font("DejaVu", "", "static/fonts/DejaVuSans.ttf")
+	pdf.AddUTF8Font("DejaVu", "B", "static/fonts/DejaVuSans-Bold.ttf")
+	pdf.AddUTF8Font("DejaVu", "I", "static/fonts/DejaVuSans-Oblique.ttf")
+	pdf.AddUTF8Font("DejaVu", "BI", "static/fonts/DejaVuSans-BoldOblique.ttf")
+	pdf.SetFont("DejaVu", "", 12)
 	pdf.SetFooterFunc(func() {
 		pdf.SetY(-15)
-		pdf.SetFont("Arial", "I", 8)
+		pdf.SetFont("DejaVu", "I", 8)
 		pdf.CellFormat(0, 10, "© 2025 BST - BS Thalwil, Martin Knoller Stocker. https://github.com/linuxswords/hat", "", 0, "C", false, 0, "")
 	})
 	pdf.AddPage()
@@ -28,10 +33,10 @@ func CreatePDF(db *gorm.DB, scores []models.Score) ([]byte, error) {
 		return nil, err
 	}
 
-	pdf.SetFont("Arial", "B", 16)
+	pdf.SetFont("DejaVu", "", 16)
 	pdf.Cell(40, 10, "Tournament Results")
 	pdf.Ln(10)
-	pdf.SetFont("Arial", "", 12)
+	pdf.SetFont("DejaVu", "", 12)
 	pdf.Cell(40, 10, "Tournament Name: "+tournament.Name)
 	pdf.Ln(10)
 	pdf.Cell(40, 10, "Date: "+tournament.Date.Format("2006-01-02"))
@@ -43,7 +48,7 @@ func CreatePDF(db *gorm.DB, scores []models.Score) ([]byte, error) {
 	pdf.Cell(40, 10, "Handicap Set: "+tournament.HandicapSet.Name)
 	pdf.Ln(20)
 
-	pdf.SetFont("Arial", "B", 10)
+	pdf.SetFont("DejaVu", "", 10)
 	pdf.CellFormat(15, 7, "Rank", "1", 0, "C", false, 0, "")
 	pdf.CellFormat(65, 7, "Name", "1", 0, "C", false, 0, "")
 	pdf.CellFormat(30, 7, "Bow Class Code", "1", 0, "C", false, 0, "")
@@ -66,9 +71,9 @@ func CreatePDF(db *gorm.DB, scores []models.Score) ([]byte, error) {
 			return nil, err
 		}
 		if i < 3 {
-			pdf.SetFont("Arial", "B", 10)
+			pdf.SetFont("DejaVu", "B", 10)
 		} else {
-			pdf.SetFont("Arial", "", 10)
+			pdf.SetFont("DejaVu", "", 10)
 		}
 		pdf.CellFormat(15, 6, fmt.Sprintf("%d", score.Ranking), "1", 0, "C", false, 0, "")
 		pdf.CellFormat(65, 6, fmt.Sprintf("%s %s", archer.FirstName, archer.LastName), "1", 0, "L", false, 0, "")
@@ -86,7 +91,7 @@ func CreatePDF(db *gorm.DB, scores []models.Score) ([]byte, error) {
 		return nil, err
 	}
 	pdf.SetY(-30)
-	pdf.SetFont("Arial", "I", 8)
+	pdf.SetFont("DejaVu", "I", 8)
 	pdf.CellFormat(0, 10, "© 2025 BST - BS Thalwil, Martin Knoller Stocker. https://github.com/linuxswords/hat", "", 0, "C", false, 0, "")
 	return buf.Bytes(), nil
 }
