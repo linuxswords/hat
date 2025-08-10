f80f34f7a0e368a6d664b041cb1f0634e43d07c0
Martin Knoller Stocker <linuxswords@gmail.com>
2025-08-10 21:40:13 +0200

    delete all

--- ops/infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "kubernetes_cluster_name" {
-  value = google_container_cluster.primary.name
-}
-
-output "kubernetes_cluster_endpoint" {
-  value = google_container_cluster.primary.endpoint
-}
--- ops/infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The GCP project ID"
-}
-
-variable "region" {
-  description = "The GCP region"
-  default     = "us-central1"
-  type = "string"
-}
